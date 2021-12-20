1c1d3db69919db0e5ad384ecfb7274f231978b21
Cansu-1907 <56121553+Cansu-1907@users.noreply.github.com>
2021-12-20 14:23:28 +0100

    /users (post request) | /users:userID (get request) completed

--- api/routes/users.js
+++ api/routes/users.js
@@ -23,4 +23,46 @@ router.get("/", (req, res) => {
     });
 });
 
+router.post("/", (req, res) => {
+  const user = new User({
+    _id: new mongoose.Types.ObjectId(),
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  user
+    .save()
+    .then((result) => console.log(result))
+    .catch((err) => console.log(err));
+
+  res.status(201).json({
+    message: "Handling POST request to /users",
+    createdUser: user,
+  });
+});
+
+router.get("/:userID", (req, res, next) => {
+  const id = req.params.userID;
+
+  User.findById(id)
+    .exec()
+    .then((doc) => {
+      console.log("From database", doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res
+          .status(404)
+          .json({ message: "No valid entry found for provided ID" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+
 module.exports = router;
